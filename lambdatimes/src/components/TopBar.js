3a60d065c73b70284d84bd7de7dd732b3bde750d
Dan Sample <danssample@gmail.com>
2019-06-01 14:14:00 -0700

    refactored styling for header and topbar

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,37 @@
 import React from 'react';
+import {
+  PopBar,
+  Container,
+  Span,
+  ContainerLeft,
+  ContainerRight,
+  ContainerCenter
+} from '../components/Styles/TopBarStylePackage';
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <PopBar>
+      <Container>
+        <ContainerLeft>
+          <Span left>TOPICS</Span>
+          <Span left>SEARCH</Span>
+        </ContainerLeft>
+        <ContainerCenter>
+          <Span>GENERAL</Span>
+          <Span>BROWNBAG</Span>
+          <Span>RANDOM</Span>
+          <Span>MUSIC</Span>
+          <Span lastChild>ANNOUNCEMENTS</Span>
+        </ContainerCenter>
+        <ContainerRight>
+          <Span right>LOG IN</Span>
+        </ContainerRight>
+      </Container>
+    </PopBar>
+  );
+};
 
 export default TopBar;
