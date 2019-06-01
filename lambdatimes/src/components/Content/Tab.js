56d3e3d089003eb0c0ac66bda5a7500646370601
Dan Sample <danssample@gmail.com>
2019-06-01 11:12:34 -0700

    Tabs displaying correctly

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Tab = props => {
-  if (props.tab === props.selectedTab) {
-  }
+  const className = props.tab === props.selectedTab ? 'tab active-tab' : 'tab';
+
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
+      className={className}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
