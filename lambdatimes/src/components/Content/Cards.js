7467ef5d2f18157ddf9ad2dd7c03fb99a67884dd
Dan Sample <danssample@gmail.com>
2019-06-01 10:48:34 -0700

    diplayed cards and their information

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -4,11 +4,13 @@ import Card from './Card';
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
+      {props.cards.map(c => (
+        <Card card={c} />
+      )) /* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
     </div>
-  )
-}
+  );
+};
 
 // Make sure you include prop types for all of your incoming props
 
